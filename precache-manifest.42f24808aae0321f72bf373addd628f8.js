48ad403d0a0ee96721d283b010fa129089661b1d
Gursimran Kaur Saini <sainigursimrankaur@gmail.com>
2020-06-19 23:02:51 +0530

    Updates

---
+++ precache-manifest.42f24808aae0321f72bf373addd628f8.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cacfec418335ecaaabbc8e9810949ad4",
+    "url": "/portfolio-website/index.html"
+  },
+  {
+    "revision": "977b2559cd6f625d0d4c",
+    "url": "/portfolio-website/static/css/main.8cf7e9cc.chunk.css"
+  },
+  {
+    "revision": "228a8e82e2fe3579a894",
+    "url": "/portfolio-website/static/js/2.5b617341.chunk.js"
+  },
+  {
+    "revision": "0311aa83fad0c839fa0d630ea3d0368e",
+    "url": "/portfolio-website/static/js/2.5b617341.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "977b2559cd6f625d0d4c",
+    "url": "/portfolio-website/static/js/main.0cad3225.chunk.js"
+  },
+  {
+    "revision": "a74d104f0088369e7888",
+    "url": "/portfolio-website/static/js/runtime-main.d0dba410.js"
+  },
+  {
+    "revision": "126036da8c9bbebc17fac1ce7f267c42",
+    "url": "/portfolio-website/static/media/ADS(COC).126036da.pdf"
+  },
+  {
+    "revision": "0c988a1f9fb22e1e757fe59f6f19dcb6",
+    "url": "/portfolio-website/static/media/Flask(COC).0c988a1f.pdf"
+  },
+  {
+    "revision": "f49069f0bae643c148354c28a3937caf",
+    "url": "/portfolio-website/static/media/International Cyber Conflicts (COC).f49069f0.pdf"
+  },
+  {
+    "revision": "d73327e03c3caa0fc36d1bdad19bcc41",
+    "url": "/portfolio-website/static/media/MLBasics(COC).d73327e0.pdf"
+  },
+  {
+    "revision": "1721d7cd02e4a2993032cddac83c478f",
+    "url": "/portfolio-website/static/media/MongoDB(COC).1721d7cd.pdf"
+  },
+  {
+    "revision": "d84c2314c699d5a8445e0510ac0e195f",
+    "url": "/portfolio-website/static/media/React(COC).d84c2314.pdf"
+  },
+  {
+    "revision": "5b35b6deaec4ef0c4ed6b376f374543b",
+    "url": "/portfolio-website/static/media/SQL(COC).5b35b6de.pdf"
+  },
+  {
+    "revision": "c04c44f21224fe78d1566da2963a7459",
+    "url": "/portfolio-website/static/media/aboutMe.c04c44f2.svg"
+  },
+  {
+    "revision": "fce199e96fa50886f96dd527df1e15fb",
+    "url": "/portfolio-website/static/media/baking.fce199e9.svg"
+  },
+  {
+    "revision": "3394874c782205302cc6e39f4b12c89a",
+    "url": "/portfolio-website/static/media/ccs.3394874c.svg"
+  },
+  {
+    "revision": "1cf733fe3efd458729a5e1f6b92f4aab",
+    "url": "/portfolio-website/static/media/certificationsOrSkills.1cf733fe.svg"
+  },
+  {
+    "revision": "19e338fe06bd9ec04f6c25d66c17bf1c",
+    "url": "/portfolio-website/static/media/coder.19e338fe.svg"
+  },
+  {
+    "revision": "908650b26c4e0da36664bf8365de4a13",
+    "url": "/portfolio-website/static/media/coding.908650b2.svg"
+  },
+  {
+    "revision": "24c7593055f87a8a6eca77a52bdbae82",
+    "url": "/portfolio-website/static/media/college.24c75930.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio-website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "25ed82d7775b563b2bb29b524d82b272",
+    "url": "/portfolio-website/static/media/matriculation.25ed82d7.svg"
+  },
+  {
+    "revision": "6a746990f5983b22c07c5ed26c119100",
+    "url": "/portfolio-website/static/media/senSecEdu.6a746990.svg"
+  },
+  {
+    "revision": "b73d3a3e3025f3da39b3adeda0e63ee9",
+    "url": "/portfolio-website/static/media/tff.b73d3a3e.svg"
+  }
+]);
